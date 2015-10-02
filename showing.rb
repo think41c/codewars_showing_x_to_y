0488e65035a578a557568a7668b4744da9b50a8a
Tony DiNitto <think41c@gmail.com>
2015-10-01 22:49:47 -0500

    Sample output represented.

--- showing.rb
+++ showing.rb
@@ -5,3 +5,18 @@
 # In a category of 30 products with 10 products per page, on page 1 you would see
 # 'Showing 1 to 10 of 30 Products.'
 
+class Showing
+	def initialize(total_products, products_per_page, user_page)
+		@total_products = total_products 
+		@products_per_page = products_per_page 
+		@user_page = user_page 
+		current_page
+	end
+
+	def current_page
+		puts "You're on page 1. Showing 1 to 10 of 50 products"
+	end
+end
+
+page = Showing.new(50, 10, 1)  # Should return "Showing 1-10 Products of 50 products"
+# Products, Products per Page, and Page user is On. 
