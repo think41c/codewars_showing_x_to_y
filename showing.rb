d518e281223ba46628f40594883d6d6eab105fa6
Tony DiNitto <think41c@gmail.com>
2015-10-01 22:32:55 -0500

    Initial commit

---
+++ showing.rb
@@ -0,0 +1,7 @@
+# A category page displays a set number of products per page, with pagination at the bottom 
+# allowing the user to move from page to page.
+# Given that you know the page you are on, how many products are in the category in total, 
+# and how many products are on any given page, how would you output a simple string showing which products you are viewing..
+# In a category of 30 products with 10 products per page, on page 1 you would see
+# 'Showing 1 to 10 of 30 Products.'
+
